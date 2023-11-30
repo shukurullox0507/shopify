ac24a49df4cf33b1f5c4bd15cfe415d5c54998a1
Shukurullox <109385687+shukurullox0507@users.noreply.github.com>
2023-11-30 17:59:13 +0500

    main page and modal

---
+++ src/components/product.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { ProductType } from '@/interfaces'
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { FC } from 'react'
+import CustomImage from './image';
+
+const Product: FC<{ product: ProductType }> = ({ product }) => {
+
+    return (
+        <Link href={`/products/${product.id}`}  className="h-96 p-6 flex flex-col rounded-lg group hover:scale-105 transition-transform ease-out duration-200 border">
+            <div className='relative max-h-96 flex-1 '>
+                <CustomImage product={product} fill/>
+            </div>
+            <h3 className="tracking-widest mt-5 text-indigo-500 text-xs font-medium title-font">{product.category}</h3>
+            <div className='font-semibold flex items-center justify-between mt-4 mb-1'>
+                <p className='w-44 truncate'>{product.title}</p>
+                <p>${product.price}</p>
+            </div>
+            <p className="leading-relaxed text-base line-clamp-2">{product.description}</p>
+        </Link>
+    )
+}
+
+export default Product
